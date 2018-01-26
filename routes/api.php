196af97a1ec3dd775c3d9507a611bb247bd3576f
Stavroula Bochrini <s.bochrini@gmail.com>
2018-01-26 11:27:48 +0200

    add plan

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,15 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-Route::post('api/plans','PlansController@store');
-Route::post('api/users','UsersController@store');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('api/plans','PlansController');
+//Route::post('api/plans','PlansController@store');
+Route::get('api/addplanform','PlansController@addplanform');
+Route::get('api/addexerisedropdown','PlansController@addexerisedropdown');
+
+Route::resource('api/users','UsersController');
+//Route::post('api/users/store','PlansController@store');
+Route::get('api/addusersform','UsersController@addusersform');
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('api/plans','PlansController');
 //Route::post('api/plans','PlansController@store');
